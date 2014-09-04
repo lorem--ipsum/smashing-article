eab2b2db76b50f95ec56c6e451e019fee6d45f7c
lorem--ipsum <vaporzub@gmail.com>
2014-09-04 10:43:09 -0700

    Added timeout test case

---
+++ src/timeout.js
@@ -0,0 +1,15 @@
+angular.module('timeouts', [])
+
+.factory('waiter', ['$timeout', function($timeout) {
+  return {
+    brieflySetSomethingToTrue: function(target, property) {
+      var oldValue = target[property];
+
+      target[property] = true;
+
+      $timeout(function() {
+        target[property] = oldValue;
+      }, 100);
+    }
+  };
+}]);
---
+++ test/timeouts.mocha.js
@@ -0,0 +1,22 @@
+describe('timeouts', function() {
+
+  beforeEach(module('timeouts'));
+
+  var waiter;
+
+  beforeEach(inject(function(_waiter_) {
+    waiter = _waiter_;
+  }));
+
+  it('should briefly set somethign to true', inject(function($timeout) {
+    var anyObject = {foo: 42};
+
+    waiter.brieflySetSomethingToTrue(anyObject, 'foo');
+
+    expect(anyObject.foo).to.equal(true);
+
+    $timeout.flush();
+
+    expect(anyObject.foo).to.equal(42);
+  }));
+});
